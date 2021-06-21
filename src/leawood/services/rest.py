2a13d13e46a4b72a330994be4f01731577577c9c
SteveMayze <smayze@yahoo.com>
2021-06-21 19:34:43 +0200

    Starts to add the REST interaction

---
+++ src/leawood/services/rest.py
@@ -0,0 +1,50 @@
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Rest:
+
+    def __init__(self, config):
+        self.config = config
+
+
+    def get(self, resource, query = None):
+        ## Get the device details
+        log = logger
+        config = self.config
+        payload={}
+        base_url = config.config_data['rest']
+        headers = { f'Authorization': 'Basic {url_auth}'}
+        if not query:
+            url = f'{base_url}/{resource}'
+        else:
+            url = f'{base_url}/{resource}?q={query}'
+        log.info (f'GET {url}\n  query: {query}')
+        response =  requests.request("GET", url, headers=headers, data=payload, timeout=30, auth=HTTPBasicAuth(config.config_data['username'], config.config_data['password']))
+        log.info(f"response: {response.json()['items'][0]}")
+        return response.json()['items'][0]
+
+    def post(self, resource, payload):
+        ## Get the device details
+        log = self.log
+        config = self.config
+        base_url = config.config_data['rest']
+        headers = {
+            'Content-Type': 'application/json',
+        ## f'Authorization': 'Basic {url_auth}'
+        }
+        url = f'{base_url}/{resource}'
+        log.info (f'POST {url}\n   payload: {payload}\n user {config.config_data["username"]}, pwd: {config.config_data["password"]}')
+        return requests.request(
+            "POST", url, headers=headers, data=payload, timeout=30, 
+            auth=HTTPBasicAuth(config.config_data['username'], config.config_data['password'])
+        )
+
+
+        
+
