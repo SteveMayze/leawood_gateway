1f8cda7741584c7e59c3abc123f23c4b0ccc951a
Steve Mayze <smayze@yahoo.com>
2021-07-24 20:03:04 +0200

    Removes the extra test options. These are no longer needed

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -8,28 +8,22 @@ from leawood.adapters.xbee import XBeeModem
 import pytest
 import os
 
-def pytest_addoption(parser):
-    parser.addoption("--xbeeport", action='store', help='Set the serial/com port for the coordinator device', default='COM6')
-    parser.addoption("--sensorxbeeport", action='store', help='Set the serial/com port for the sensor device', default='COM7')
 
-
-def create_config(pytestconfig, configname, portname):
-    port = pytestconfig.getoption(portname)
+def create_config(pytestconfig, configname):
     script_dir = os.path.dirname(__file__)
     config_path = os.path.join(script_dir, configname)
     cert_path = os.path.join(script_dir, '.ssh')
-    args = ['--serial-port', port, 
-        '--config', config_path, 
+    args = ['--config', config_path, 
         '--certpath', cert_path]
     return Config(args)
 
 @pytest.fixture
 def config(pytestconfig):
-    return create_config(pytestconfig, 'config.json', 'xbeeport')
+    return create_config(pytestconfig, 'config.json')
 
 @pytest.fixture
 def sensor(pytestconfig):
-    sensor_config = create_config(pytestconfig, 'config.json', 'sensorxbeeport')
+    sensor_config = create_config(pytestconfig, 'config-test-sensor.json')
     message_bus = LocalMessageBus()
     modem = XBeeModem(sensor_config)
     sensor = Sensor(message_bus, modem)
