f66dd6d39794afe03167d8f5e4cdbbde0ae0b15a
SteveMayze <smayze@yahoo.com>
2023-01-09 20:23:14 +0100

    More changes

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -6,18 +6,18 @@ REPO_URL='https://github.com/SteveMayze/leawood_gateway.git'
 def deploy():
     site_folder = f'/home/{env.user}/python/leawood_gateway'
     run(f'mkdir -p {site_folder}')
+    _stop_gateway()
     with cd(site_folder):
-        _stop_gateway()
         _get_last_source()
         _update_virtualenv()
         _create_or_update_config()
         _create_or_update_daemon()
-        _start_the_gateway()
+    _start_the_gateway()
 
 def _stop_gateway():
     if exists('src/leawood'):
         if  exists('/etc/init.d/leawood-gateway'):
-            run('/etc/init.d/leawood-gateway stop')
+            sudo('/etc/init.d/leawood-gateway stop', shell=False)
         else:
             run('.venv/bin/python -m leawood stop')        
 
@@ -43,12 +43,12 @@ def _create_or_update_config():
 
 def _start_the_gateway():
     ### run('nohup .venv/bin/python  -m leawood -c ~/python/leawood_gateway/config.ini start &')
-    run('sudo /etc/init.d/leawood-gateway start &')
+    sudo('/etc/init.d/leawood-gateway start &', shell=False)
 
 
 def _create_or_update_daemon():
     if  not exists('/etc/init.d/leawood-gateway'):
-        run('cp leawood-gateway /etc/init.d/')
-        run('chmod 755 /etc/init.d/leawood-gateway')
+        sudo('cp leawood-gateway /etc/init.d/', shell=False)
+        sudo('chmod 755 /etc/init.d/leawood-gateway', shell=False)
 
 
